4e04147299e52d254670b223c4270392870182bf
tarian <70381096+4masa6@users.noreply.github.com>
2022-04-11 16:03:11 +0900

    TODOの追加機能

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,30 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
-  const [incompleteTodos, setIncompleteTodos] = useState(["あああ", "いいい"]);
-  const [completeTodos, setCompleteTodos] = useState(["ううう"]);
+  const [todoText, setTodoText] = useState(""); // 入力値を保持するstate
+  const [incompleteTodos, setIncompleteTodos] = useState(["あああ", "いいい"]); // 未完了のTODOを保持するstate
+  const [completeTodos, setCompleteTodos] = useState(["ううう"]); // 完了のTODOを保持するstate
+
+  // 入力値が変更されたら、その値をtodoTextにセット
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  // 追加ボタンをクリックしたら、新しい配列を作成してincompleteTodosにセット
+  const onClickAdd = () => {
+    if (todoText === "") return; // 入力値が空の場合はreturn
+    const newTodos = [...incompleteTodos, todoText]; // 今のincompleteTodosの配列に、todoTextを追加した新しい配列ができる
+    setIncompleteTodos(newTodos);
+    setTodoText(""); // フォームの値を削除する
+  };
 
   return (
     <>
       <div className="input-area">
-        <input type="text" placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          type="text"
+          value={todoText}
+          onChange={onChangeTodoText}
+          placeholder="TODOを入力"
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
