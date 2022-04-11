188f92e457f3a0482dbcefb45e9b8e881fb98257
tarian <70381096+4masa6@users.noreply.github.com>
2022-04-11 17:06:25 +0900

    画面をコンポーネントに分割

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  borderRadius: "8px",
+  padding: "8px",
+  margin: "8px"
+};
+
 export const InputTodo = (props) => {
   // propsから値を受け取る
   const { todoText, onChangeTodoText, onClickAdd } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       <input
         type="text"
         value={todoText}
